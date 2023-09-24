43144bb992d60f976de222c384c583531f828f6a
CodeWithSarim <137917202+CodeWithSarim@users.noreply.github.com>
2023-09-24 21:05:34 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,190 @@
+// Chapter 01 Answers
+
+// Question 01
+alert("First Name");
+alert("Last Name");
+alert("Email");
+alert("Phone Number");
+alert("Password");
+
+// Question 02
+alert("You are learning Javascript!");
+
+// Question 03
+alert("I am Sarim Khan");
+
+// Chapter 02 Answers
+
+// Question 01
+let firstName = console.log("Enter First Name");
+
+// Question 02
+let myName
+myName = console.log("Sarim Khan");
+
+// Question 03
+let teamName = alert("CodeWithSarim");
+
+// Question 04
+var bestMan = "charlie";
+bestMan = "Sarim";
+
+// Chapter 03 Answers
+
+// Question 01
+let caseQty;
+
+// Question 02
+caseQty = 144;
+
+// Question 03
+var num = "9";
+
+// Question 04
+let sum;
+let a = 9;
+let b = 10;
+sum = console.log(a + b);
+
+// Question 05
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+
+// Question 06
+var x = 10;
+x = x + 10;
+
+// Chapter 04 Answers
+
+// Question 01
+var productCost = 3.45;
+
+// Question 02
+var nameOfBand = "name";
+
+// Question 03
+var legallyNamed = 90;
+
+// Question 04
+var sarimKhan;
+
+// Question 05
+var myVariable;
+var _privateVar;
+var $specialVar;
+var camelCaseVariable;
+var num123;
+
+// Chapter 05 Answers
+
+// Question 01
+var name = "remainder";
+var symbol = "%";
+
+// Question 02
+value = 2;
+
+// Question 03
+var largeNum = console.log(1000 * 2000);
+
+// Question 04
+var variable1 = 10;
+var variable2 = 5;
+var result = console.log(variable1 - variable2);
+
+// Question 05
+var dividend = 10;
+var divisor = 3;
+var remainderResult = console.log(dividend % divisor);
+
+// Question 06
+alert(a * b);
+
+// Chapter 06 Answers
+
+// Question 01
+var y = 2;
+x++
+
+// Question 02
+var x1 = 100;
+x1--
+
+// Question 03
+var valueOfy = 51;
+
+// Question 04
+var valueOfz = 49;
+
+// Question 05
+var newNum = num--;
+
+// Question 06
+var originalValue = 5;
+var newValue = originalValue++
+
+// Question 07
+var num3 = 10;
+alert(num3++)
+
+// Chapter 07 Answers
+
+// Question 01 
+var value = 14;
+
+// Question 02 
+var value = 24;
+
+// Question 03
+var value = 24;
+
+// Question 04
+var value = 18;
+
+// Question 05
+var cost = (2 + 2) * (4 + 10);
+
+// Question 06
+var units = 2 + (2 * 4) + 10;
+
+// Question 07
+var pressure = (4 / 2) * 4
+
+// Chapter 08 Answers
+
+// Question 01
+var result1 = 22;
+
+// Question 02
+alert("Hello" + "Dolly");
+
+// Question 03
+var result2 = 333;
+
+// Question 04
+var part1 = "Pakistan";
+var part2 = "Zindabad";
+alert(part1 + part2);
+
+// Question 05
+var myString = "Hello, ";
+var myNumber = 42;
+var result = myString + myNumber;
+
+// Question 06
+var firstString = "Hello, ";
+var secondString = "world!";
+var combinedString = firstString + secondString;
+
+
+
+
+
+
+
+
+
+
+
+
